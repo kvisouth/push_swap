fc85694ae9ad5e72123adea134900448c60780c1
Kevin Visouthivong <kevisout@made-f0Br4s11.clusters.42paris.fr>
2024-11-30 11:26:28 +0100

    added str to int[]

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: kevisout <kevisout@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 15:17:57 by kevisout          #+#    #+#             */
-/*   Updated: 2024/11/30 11:08:03 by kevisout         ###   ########.fr       */
+/*   Updated: 2024/11/30 11:26:02 by kevisout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,14 @@
 
 # include <stdlib.h>
 # include <unistd.h>
-# include <stdio.h>
 # include <limits.h>
-# include "../libft/libft.h"
+# include <stdio.h>
+# include "../libft/libft.h"\
 
 /* Parsing */
 char	*join_args(int ac, char **av);
 long	*get_nbrs(char *str, int count);
+int     count_nbrs(char *args);
 int		check_overflows(long *nbrs, int count);
 int		check_duplicates(long *nbrs, int count);
 int		parse_args(char *args);
--- src/main.c
+++ src/main.c
@@ -6,22 +6,27 @@
 /*   By: kevisout <kevisout@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 15:17:42 by kevisout          #+#    #+#             */
-/*   Updated: 2024/11/30 11:01:09 by kevisout         ###   ########.fr       */
+/*   Updated: 2024/11/30 11:25:49 by kevisout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/push_swap.h"
-#include <stdio.h>
 
 int	main(int ac, char **av)
 {
 	char	*args;
+	long	*nbrs;
+	int		cpt;
 
 	args = join_args(ac, av);
 	if (!args)
-		return (1);
+		return (0);
 	if (!parse_args(args))
-		return (free(args), 1);
+		return (free(args), 0);
+	cpt = count_nbrs(args);
+	nbrs = get_nbrs(args, cpt);
 	free(args);
-	return (0);
+	if (!nbrs)
+		return (0);
+	return (free(nbrs), 1);
 }
