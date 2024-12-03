ab83b26578fb0c8c9f32b1aed201d92e4e93313b
Kevin Visouthivong <kevisout@made-f0Br5s8.clusters.42paris.fr>
2024-12-03 18:58:04 +0100

    added 'pa'

--- src/sort_instructions_push.c
+++ src/sort_instructions_push.c
@@ -6,7 +6,7 @@
 /*   By: kevisout <kevisout@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 17:56:25 by kevisout          #+#    #+#             */
-/*   Updated: 2024/12/03 18:53:26 by kevisout         ###   ########.fr       */
+/*   Updated: 2024/12/03 18:57:54 by kevisout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,15 @@
 /* Prend le premier élément au sommet de b et le met sur a */
 void	pa(t_stack **stack_a, t_stack **stack_b)
 {
-	(void)stack_a;
-	(void)stack_b;
+	t_stack	*tmp;
+
+	if (!*stack_b)
+		return ;
+	tmp = *stack_b;
+	*stack_b = (*stack_b)->next;
+	tmp->next = *stack_a;
+	*stack_a = tmp;
+	write(1, "pa\n", 3);
 }
 
 /* Prend le premier élément au sommet de a et le met sur b */
